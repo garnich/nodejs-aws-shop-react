199279c6fc8237b3a239e54e135d8b9a7eb96fc7
Andrei Ihnatsiuk <33061150+garnich@users.noreply.github.com>
2024-06-21 13:35:30 +0300

    Task 3 (#2)
    * task-2: updated project UI
    
    * task-2: sdk init
    
    * test-2: updated eslintrc
    
    * test-2: updated npmrc file
    
    * test-2: updated sdk-ts-stack file
    
    * test-2: updated scripts
    
    * task-3: updated configs for BE task-3
    
    * task-3: fixed queries products typo

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://0jikk5cv5i.execute-api.us-west-2.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
