12d3134db9aaac04456c2b447abedb3a8a0595c9
Andrei Ihnatsiuk <garnich@tut.by>
2024-08-03 12:11:12 +0300

    task-9: updated path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://gga03qd48g.execute-api.us-west-2.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://garnich-cart-api-develop.us-west-2.elasticbeanstalk.com/",
 };
 
 export default API_PATHS;
