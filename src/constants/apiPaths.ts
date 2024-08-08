853ac56b5b07b82980452ba2002905c5e87cc9f8
Andrei Ihnatsiuk <garnich@tut.by>
2024-08-08 16:47:50 +0300

    task-10: fixed bff path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://0jikk5cv5i.execute-api.us-west-2.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://gga03qd48g.execute-api.us-west-2.amazonaws.com/prod",
-  bff: "http://garnich-bff-api-develop.us-west-2.elasticbeanstalk.com/",
+  bff: "http://garnich-bff-api-develop.us-west-2.elasticbeanstalk.com",
   cart: "http://garnich-cart-api-develop.us-west-2.elasticbeanstalk.com/",
 };
 
